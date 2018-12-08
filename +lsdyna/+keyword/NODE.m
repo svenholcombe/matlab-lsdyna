687f5cd6b1f51aa3d3d6b1564dbb449436d9c420
svenholcombe <sven.holcombe@gmail.com>
2018-12-07 21:49:59 -0500

    Cleaned up NODE string parser to be robust to full 8-digit node ids

--- +lsdyna/+keyword/NODE.m
+++ +lsdyna/+keyword/NODE.m
@@ -38,36 +38,39 @@ classdef NODE < lsdyna.keyword.card
         
         function C = arr_stringToData(C)
             % Parse the string data and populate this card's numeric data
-            
             lineDefns = C(1).LineDefinitions;
             % There's only 1 repeated line for NODE cards. Just use it.
             FLDS = lineDefns.FLDS{1};
-            
             nFlds = size(FLDS,1);
             fmtStr = cell2mat(strcat('%', arrayfun(@num2str,FLDS.size,'Un',0), FLDS.fmt)');
             
+            % Grab and concatenate each card's active strings
             strs = cat(1,C.ActiveString);
             strsLineCounts = cellfun(@numel,{C.ActiveString});
             strs = C.convertCommaSepStrsToSpacedStrs(strs,FLDS.size);
-            %%
-            strsAsCharMat = char(strs);
-            strsAsCharMat(1,size(strsAsCharMat,2)+1:FLDS.endChar(end)) = ' ';
-            sizeBasedText = strsAsCharMat(:,1:FLDS.endChar(end))';
-            sizeBasedText(end+1:max(FLDS.endChar),:) = ' ';
             
+            % Convert strings to char matrix, truncate whitespace from long
+            % lines and fill mat to expected size with spaces and a newline
+            strsAsCharMat = char(strs);
+            strsAsCharMat(:,FLDS.endChar(end)+2:end) = [];
+            strsAsCharMat(:,FLDS.endChar(end)+1) = newline;
+            
+            % Default empty pieces to "0" for proper use in sscanf
             for i = 1:nFlds
-                emptyMask = all(sizeBasedText(FLDS.charInds{i},:) == ' ',1);
-                sizeBasedText(FLDS.endChar(i),emptyMask) = '0';
+                emptyMask = all(strsAsCharMat(:,FLDS.charInds{i}) == ' ',2);
+                strsAsCharMat(emptyMask,FLDS.endChar(i)) = '0';
             end
-            NODEDATA = reshape(sscanf(sizeBasedText,fmtStr), nFlds,[])';
+            
+            % Scanf the string and extract numeric data
+            NODEDATA = reshape(sscanf(strsAsCharMat',[fmtStr newline]), nFlds,[])';
             NODE = array2table(NODEDATA,'Var',FLDS.fld);
             NODE.nid = uint32(NODE.nid);
             NODE.tc = uint8(NODE.tc);
             NODE.rc = uint8(NODE.rc);
-            %%
+            
+            % Identify rows belonging to each individual card
             linesEndAt = cumsum(strsLineCounts);
             linesStartAt = [1 linesEndAt(1:end-1)+1];
-            
             NODEset = arrayfun(@(from,to)NODE(from:to,:),linesStartAt,linesEndAt,'Un',0);
             [C.NodeData] = NODEset{:};
         end
