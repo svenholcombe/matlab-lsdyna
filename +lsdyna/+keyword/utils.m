82c90e22336d78d3c7993e5ffab78d0c716787d9
svenholcombe <sven.holcombe@gmail.com>
2019-02-15 11:31:33 -0500

    Added 10-noded solid element card reading

--- +lsdyna/+keyword/utils.m
+++ +lsdyna/+keyword/utils.m
@@ -34,16 +34,41 @@ classdef utils
                         "ELEMENT_SHELL_THICKNESS"   2 @(i)mod(i,2)==0   FLDSshellThick
                         }, 'Var', {'keyword','lineNo','lineMatchFcn','FLDS'});
                 case "ELEMENT_SOLID"
+                    % Note: there are old and new formats for the
+                    % ELEMENT_SOLID layout. One has the 8-node in 1 line
+                    % card definition but there is also a 10-node in 2
+                    % lines cards definition. It will be the responsibility
+                    % of the ELEMENT_SOLID.arr_stringToData() function to
+                    % determine which is appropriate.
                     FLDSsolid = cell2table({
                         'eid' 'pid' 'n1' 'n2' 'n3' 'n4' 'n5' 'n6' 'n7' 'n8'
                         8         8    8    8    8    8    8    8    8    8
                         "d"     "d"  "d"  "d"  "d"  "d"  "d"  "d"  "d"  "d"
                         }','Var',{'fld','size','fmt'});
-                    % Note: this is for the 8-node in 1 card definition.
-                    % There is also a 10-node in 2 cards definition not yet
-                    % implemented.
+                    FLDSsolid10_line1 = cell2table({
+                        'eid' 'pid'
+                        8         8
+                        "d"     "d"
+                        }','Var',{'fld','size','fmt'});
+                    FLDSsolid10_line2 = cell2table({
+                        'n1' 'n2' 'n3' 'n4' 'n5' 'n6' 'n7' 'n8' 'n9' 'n10'
+                        8      8    8    8    8    8    8    8    8    8
+                        "d"   "d"  "d"  "d"  "d"  "d"  "d"  "d"  "d"  "d"
+                        }','Var',{'fld','size','fmt'});
                     defns = cell2table({
-                        "ELEMENT_SOLID"             1 @(i)true(size(i)) FLDSsolid
+                        "ELEMENT_SOLID"                     1 @(i)true(size(i)) FLDSsolid
+                        "ELEMENT_SOLID (ten nodes format)"  1 @(i)mod(i,2)==1   FLDSsolid10_line1
+                        "ELEMENT_SOLID (ten nodes format)"  2 @(i)mod(i,2)==0   FLDSsolid10_line2
+                        }, 'Var', {'keyword','lineNo','lineMatchFcn','FLDS'});
+                case "ELEMENT_DISCRETE"
+                    case "NODE"
+                    FLDS = cell2table({
+                        'nid' 'pid' 'n1' 'n2' 'vid' 's' 'pf' 'offset'
+                        8      8     8    8    8    16   8    16
+                        "d"   "d"   "d"  "d"  "d"  "f"  "d"   "f"
+                        }','Var',{'fld','size','fmt'});
+                    defns = cell2table({
+                        "NODE" 1 @(i)true(size(i)) FLDS
                         }, 'Var', {'keyword','lineNo','lineMatchFcn','FLDS'});
             end
             
